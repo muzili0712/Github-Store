e516f652291a99a387b9bc670c088f7c548e8f9b
rainxchzed <undefineduser087@gmail.com>
2026-05-03 11:31:14 +0500

    fix: order auth-state updates by response and reset on every non-401

--- core/data/src/commonMain/kotlin/zed/rainxch/core/data/network/interceptor/UnauthorizedInterceptor.kt
+++ core/data/src/commonMain/kotlin/zed/rainxch/core/data/network/interceptor/UnauthorizedInterceptor.kt
@@ -5,17 +5,13 @@ import io.ktor.client.plugins.HttpClientPlugin
 import io.ktor.client.statement.HttpReceivePipeline
 import io.ktor.http.HttpHeaders
 import io.ktor.util.AttributeKey
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import zed.rainxch.core.domain.repository.AuthenticationState
 
 class UnauthorizedInterceptor(
     private val authenticationState: AuthenticationState,
-    private val scope: CoroutineScope,
 ) {
     class Config {
         var authenticationState: AuthenticationState? = null
-        var scope: CoroutineScope? = null
     }
 
     companion object Plugin : HttpClientPlugin<Config, UnauthorizedInterceptor> {
@@ -29,10 +25,6 @@ class UnauthorizedInterceptor(
                     requireNotNull(config.authenticationState) {
                         "AuthenticationState must be provided"
                     },
-                scope =
-                    requireNotNull(config.scope) {
-                        "CoroutineScope must be provided"
-                    },
             )
         }
 
@@ -42,18 +34,10 @@ class UnauthorizedInterceptor(
         ) {
             scope.receivePipeline.intercept(HttpReceivePipeline.After) {
                 val tokenKey = extractBearerToken(subject.call.request.headers[HttpHeaders.Authorization])
-                val statusCode = subject.status.value
-                when {
-                    statusCode == 401 -> {
-                        plugin.scope.launch {
-                            plugin.authenticationState.notifySessionExpired(tokenKey)
-                        }
-                    }
-                    statusCode in 200..299 -> {
-                        plugin.scope.launch {
-                            plugin.authenticationState.notifyRequestSucceeded(tokenKey)
-                        }
-                    }
+                if (subject.status.value == 401) {
+                    plugin.authenticationState.notifySessionExpired(tokenKey)
+                } else {
+                    plugin.authenticationState.notifyRequestSucceeded(tokenKey)
                 }
                 proceedWith(subject)
             }
